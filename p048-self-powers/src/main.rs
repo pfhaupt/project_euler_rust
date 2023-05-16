65b56776b98b7b9ec991da091fad5271297bf494
MrLagSux <pfhaupt@gmx.net>
2023-05-16 23:58:42 +0200

    Add Problem 48

---
+++ p048-self-powers/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // https://projecteuler.net/problem=48
+    const DIGIT_COUNT: u32 = 10;
+    const POW10: u64 = 10u64.pow(DIGIT_COUNT);
+    let mut result = 0;
+    for n in 1..=1000 {
+        let mut sum: u64 = 1;
+        for _ in 0..n {
+            sum = (sum * n) % POW10;
+        }
+        result = (result + sum) % POW10;
+    }
+    println!("{}", result);
+}
