53b4252499f21f86c6d19de8cf7696d4aadf54ef
MrLagSux <pfhaupt@gmx.net>
2023-05-16 23:49:39 +0200

    Add Problem 47

---
+++ p047-distinct-primes-factors/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    // https://projecteuler.net/problem=47
+    const STREAK: u64 = 4;
+    let mut max = 11;
+    let mut primes = calc_primes(vec![2], 3, 11);
+    let mut current = 1;
+    loop {
+        if 2 * current > max {
+            let new_limit = 2 * max + 1;
+            primes = calc_primes(primes, max, new_limit);
+            max = new_limit;
+        }
+        let mut valid = true;
+        for i in 0..STREAK {
+            let n = current + i;
+            let prime_factors = get_unique_prime_factors(n, &primes);
+            if prime_factors.len() as u64 != STREAK {
+                valid = false;
+                break;
+            }
+        }
+        if valid {
+            break;
+        }
+        current += 1;
+    }
+    println!("{}", current);
+}
+
+fn calc_primes(old_primes: Vec<u64>, from: u64, limit: u64) -> Vec<u64> {
+    let mut primes: Vec<u64> = old_primes;
+    for number in (from..limit).step_by(2) {
+        let sqrt = f64::sqrt(number as f64) as u64;
+        let mut prime = true;
+        for p in &primes {
+            if p > &sqrt {
+                break;
+            } else if number % p == 0 {
+                prime = false;
+                break;
+            }
+        }
+        if prime {
+            primes.push(number);
+        }
+    }
+    primes
+}
+
+fn get_unique_prime_factors(number: u64, primes: &Vec<u64>) -> Vec<u64> {
+    let mut result = vec![];
+    for p in primes {
+        if number % p == 0 {
+            result.push(*p);
+        }
+    }
+    result
+}
