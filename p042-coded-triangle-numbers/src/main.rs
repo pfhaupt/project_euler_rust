43c00ded34b0ddd386d7e2cf0a9d048bff2fdf00
MrLagSux <pfhaupt@gmx.net>
2023-05-16 18:33:25 +0200

    Add Problem 42

---
+++ p042-coded-triangle-numbers/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+    let triangle_numbers: HashSet<usize> = (1..1000).into_iter().map(|n|n*(n+1)/2).collect();
+    let contents = fs::read_to_string("src/words.txt")
+        .expect("Something went wrong when loading the file!");
+    let names: Vec<&str> = contents.split(",").collect();
+
+    let mut edited: Vec<&str> = vec![];
+    for name in names {
+        let mut iter = name.chars();
+        //Skip the quotes
+        iter.next();
+        iter.next_back();
+        edited.push(iter.as_str());
+    }
+    let result = edited.iter()
+                        .map(|name| is_triangle_word(*name, &triangle_numbers))
+                        .filter(|b| *b)
+                        .collect::<Vec<bool>>()
+                        .len();
+    println!("{:?}", result);
+}
+
+fn is_triangle_word(word: &str, triangle_numbers: &HashSet<usize>) -> bool {
+    let chars = word.as_bytes();
+    let sum: usize = chars.iter().map(|ch| (*ch as usize) - 64).sum();
+    triangle_numbers.contains(&sum)
+}
