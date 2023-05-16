bf819d28ad795c49529293c5f3028f0b98545955
MrLagSux <pfhaupt@gmx.net>
2023-05-16 17:08:49 +0200

    Add Problem 40

---
+++ p040-champernowne-constant/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // https://projecteuler.net/problem=40
+    const MAX: usize = 1000000;
+    let mut fraction = String::from("");
+    let mut number = 1;
+    while fraction.len() < MAX {
+        fraction += &number.to_string();
+        number += 1;
+    }
+    let fraction = fraction.as_bytes();
+    let mut result = 1;
+    let mut index = 1;
+    while index <= MAX {
+        result *= (fraction[index - 1] as usize) - 48;
+        index *= 10;
+    }
+    println!("{}", result);
+}
