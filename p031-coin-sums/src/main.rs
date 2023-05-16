0e860c41ed4d4ff1fd19cf537a222efc4a3efdea
MrLagSux <pfhaupt@gmx.net>
2023-05-16 22:49:06 +0200

    Make Problem 31 more flexible to GOAL

--- p031-coin-sums/src/main.rs
+++ p031-coin-sums/src/main.rs
@@ -1,14 +1,12 @@
 
 const GOAL: u32 = 200;
 // UK
-const COURSE: [u32; 8] = [200, 100, 40, 20, 10, 4, 2, 1];
 const WORTH: [u32; 8] = [1, 2, 5, 10, 20, 50, 100, 200];
 // US
-// const COURSE: [u32; 7] = [200, 40, 20, 8, 4, 2, 1];
 // const WORTH: [u32; 7] = [1, 5, 10, 25, 50, 100, 200];
 fn main() {
     // https://projecteuler.net/problem=31
-    let result = check_balance(COURSE.len() as i32 - 1, 0);
+    let result = check_balance(WORTH.len() as i32 - 1, 0);
     println!("{}", result);
 }
 
@@ -18,7 +16,8 @@ fn check_balance(depth: i32, acc: u32) -> u32 {
     }
     let depth = depth as usize;
     let mut valid = 0;
-    for i in 0..=COURSE[depth] {
+    let amount = GOAL / &WORTH[depth];
+    for i in 0..=amount {
         let this_sum = acc + i * &WORTH[depth];
         if this_sum == GOAL {
             valid += 1;
