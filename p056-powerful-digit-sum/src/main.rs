b38c75bcb77e9a1d857baf0232768431e542a573
MrLagSux <pfhaupt@gmx.net>
2023-05-18 23:52:46 +0200

    Add Problem 56

---
+++ p056-powerful-digit-sum/src/main.rs
@@ -0,0 +1,22 @@
+use num_bigint::BigUint;
+
+fn main() {
+    // https://projecteuler.net/problem=56
+    let mut record = BigUint::from(0u32);
+    for a in 1..100u32 {
+        for b in 1..100u32 {
+            let base = BigUint::from(a);
+            let mut pow = base.pow(b);
+            let mut digit_sum = BigUint::from(0u32);
+            while pow != BigUint::from(0u32) {
+                let digit = &pow % 10u32;
+                digit_sum += digit;
+                pow /= 10u32;
+            }
+            if digit_sum > record {
+                record = digit_sum;
+            }
+        }
+    }
+    println!("{}", record);
+}
