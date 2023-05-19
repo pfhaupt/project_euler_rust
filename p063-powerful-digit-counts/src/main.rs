e48430b3ef5781f16391cfd87995e37154ca777b
MrLagSux <pfhaupt@gmx.net>
2023-05-20 01:11:05 +0200

    Add Problem 63

---
+++ p063-powerful-digit-counts/src/main.rs
@@ -0,0 +1,23 @@
+use std::iter::successors;
+
+fn main() {
+    // https://projecteuler.net/problem=63
+    let mut n = 1;
+    let mut result = 0;
+    loop {
+        let mut new_powers = 0;
+        for digit in 1..10u128 {
+            let pow = digit.pow(n);
+            let digit_count = successors(Some(pow), |&n| (n >= 10).then(|| n / 10)).count() as u32;
+            if digit_count == n {
+                new_powers += 1;
+            }
+        }
+        if new_powers == 0 {
+            break;
+        }
+        result += new_powers;
+        n += 1;
+    }
+    println!("{}", result);
+}
