e4abb2bea73cb7a408d718580e257a6737cc611e
MrLagSux <pfhaupt@gmx.net>
2023-05-25 21:50:41 +0200

    Add Problem 74

---
+++ p074-digit-factorial-chains/src/main.rs
@@ -0,0 +1,40 @@
+// https://projecteuler.net/problem=74
+
+fn main() {
+    const LIMIT: usize = 1_000_000;
+    let lookup_factorials: Vec<usize> = (0..10).map(|n| factorial(n)).collect();
+    let mut result = 0;
+    for n in 1..LIMIT {
+        let mut values = vec![n];
+        let mut current = n;
+        loop {
+            let next = apply_rule(current, &lookup_factorials);
+            if values.contains(&next) {
+                break;
+            }
+            values.push(next);
+            current = next;
+        }
+        if values.len() == 60 {
+            result += 1;
+        }
+    }
+    println!("{}", result);
+}
+
+fn apply_rule(mut n: usize, lookup: &Vec<usize>) -> usize {
+    let mut sum = 0;
+    while n != 0 {
+        sum += lookup[n % 10];
+        n /= 10;
+    }
+    sum
+}
+
+fn factorial(n: usize) -> usize {
+    let mut result = 1;
+    for i in 1..=n {
+        result *= i;
+    }
+    result
+}
