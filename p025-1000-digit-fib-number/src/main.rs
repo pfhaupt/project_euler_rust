33f78cd15c8c4270507f256e7062b37309921119
MrLagSux <pfhaupt@gmx.net>
2023-05-29 21:50:50 +0200

    Remove debug info in Problem 25

--- p025-1000-digit-fib-number/src/main.rs
+++ p025-1000-digit-fib-number/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     const GOAL: usize = 1000;
     let mut a = BigUint::from(0u32);
     let mut b = BigUint::from(1u32);
-    let mut c = BigUint::from(1u32);
+    let mut c;
     let mut digit_count = 1;
     let mut index = 0;
     while digit_count < GOAL {
@@ -16,5 +16,5 @@ fn main() {
         digit_count = c.to_string().bytes().len();
         index += 1;
     }
-    println!("{} {}", index, c);
+    println!("{}", index);
 }
