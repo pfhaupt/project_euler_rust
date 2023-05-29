6e31d10019a0a5573104a542c84bdca558108587
MrLagSux <pfhaupt@gmx.net>
2023-05-29 21:49:18 +0200

    Remove debug info in Problem 03

--- p003-prime-fac/src/main.rs
+++ p003-prime-fac/src/main.rs
@@ -2,12 +2,14 @@
 
 fn main() {
     let mut n: i64 = 600851475143;
+    let mut biggest_factor = 0;
     while n != 1 {
         let mut factor = 2;
         while n % factor != 0 {
             factor += 1;
         }
         n = n / factor;
-        println!("{}", factor);
+        biggest_factor = biggest_factor.max(factor);
     }
+    println!("{}", biggest_factor);
 }
