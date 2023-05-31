c345cd3ed042ae98d3fc5bc652fc9331e42343a1
MrLagSux <pfhaupt@gmx.net>
2023-05-31 13:53:22 +0200

    Optimize Problem 60

--- p060-prime-pair-sets/src/main.rs
+++ p060-prime-pair-sets/src/main.rs
@@ -96,15 +96,17 @@ fn calc_primes(old_primes: Vec<u64>, mut from: u64, limit: u64) -> Vec<u64> {
     primes
 }
 
-fn is_prime(candidate: u64) -> bool {
-    if candidate == 1 || candidate % 2 == 0 {
-        return false;
+fn is_prime(n: u64) -> bool {
+    if n == 2 || n == 3 { return true; }
+    if n < 2 || n % 2 == 0 { return false; }
+    if n < 9 { return true; }
+    if n % 3 == 0 { return false; }
+    let r = f64::sqrt(n as f64) as u64;
+    let mut f = 5;
+    while f <= r {
+        if n % f == 0 { return false; }
+        if n % (f + 2) == 0 { return false; } 
+        f += 6;
     }
-    let sqrt = f64::sqrt(candidate as f64) as u64;
-    for n in (3..=sqrt).step_by(2) {
-        if candidate % n == 0 {
-            return false;
-        }
-    }
-    true
+    return true;
 }
