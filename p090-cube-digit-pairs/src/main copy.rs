1e1110460faf13124bbb9a270a11f009f33deab8
MrLagSux <pfhaupt@gmx.net>
2023-06-13 21:53:50 +0200

    Add Problem 90

--- p090-cube-digit-pairs/src/main copy.rs
+++ p090-cube-digit-pairs/src/main copy.rs
@@ -5,20 +5,16 @@ use std::time::Instant;
 fn main() {
     let now = Instant::now();
     let arrangements = get_arrangements();
-    let squares = (1..10).map(|x| {
-        let s = x * x;
-        let mut s1 = s % 10;
-        let mut s2 = s / 10;
-        if s1 == 9 { s1 = 6; }
-        if s2 == 9 { s2 = 6; }
-        s2 * 10 + s1
-    }).collect();
+    let squares = (1..10).map(|x|x*x).collect::<Vec<u8>>();
+    println!("{:?}", squares);
+    println!("{}", arrangements.len());
     let mut valid = 0;
     for i in 0..arrangements.len() {
         let arr1 = &arrangements[i];
         for j in (i+1)..arrangements.len() {
             let arr2 = &arrangements[j];
             if is_hit(&arr1, &arr2, &squares) {
+                println!("{:?}, {:?}", arr1, arr2);
                 valid += 1;
             }
         }
@@ -41,6 +37,15 @@ fn is_hit(arr1: &Vec<u8>, arr2: &Vec<u8>, squares: &Vec<u8>) -> bool {
                 if (d1 == s1 && d2 == s2) || (d1 == s2 && d2 == s1) {
                     hit[s_i] = true;
                 }
+                if d1 == 6 {
+                    if (9 == s1 && d2 == s2) || (9 == s2 && d2 == s1) {
+                        hit[s_i] = true;
+                    }
+                } else if d2 == 6 {
+                    if (d1 == s1 && 9 == s2) || (d1 == s2 && 9 == s1) {
+                        hit[s_i] = true;
+                    }
+                }
             }
         }
     }
@@ -49,12 +54,12 @@ fn is_hit(arr1: &Vec<u8>, arr2: &Vec<u8>, squares: &Vec<u8>) -> bool {
 
 fn get_arrangements() -> Vec<Vec<u8>> {
     let mut result = vec![];
-    for a in 0..=9 {
-        for b in (a+1)..=9 {
-            for c in (b+1)..=9 {
-                for d in (c+1)..=9 {
-                    for e in (d+1)..=9 {
-                        for f in (e+1)..=9 {
+    for a in 0..9 {
+        for b in (a+1)..9 {
+            for c in (b+1)..9 {
+                for d in (c+1)..9 {
+                    for e in (d+1)..9 {
+                        for f in (e+1)..9 {
                             result.push(vec![a, b, c, d, e, f]);
                         }
                     }
@@ -62,9 +67,5 @@ fn get_arrangements() -> Vec<Vec<u8>> {
             }
         }
     }
-    result.iter().map(|v| {
-        v.iter().map(|&e| {
-            if e == 9 { 6 } else { e }
-        }).collect()
-    }).collect()
+    result
 }
