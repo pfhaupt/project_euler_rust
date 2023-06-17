87781aa4396e83ce6816afa61719da62b34f24bf
MrLagSux <pfhaupt@gmx.net>
2023-06-17 12:52:05 +0200

    Optimize Problem 07

--- p007-calc-prime/src/main.rs
+++ p007-calc-prime/src/main.rs
@@ -4,8 +4,8 @@ use std::time::Instant;
 
 fn main() {
     let now = Instant::now();
-    let mut prime_counter = 0;
-    let mut n = 2;
+    let mut prime_counter = 1;
+    let mut n = 3;
     loop {
         if is_prime(n) {
             prime_counter += 1;
@@ -15,15 +15,23 @@ fn main() {
             println!("{:?}", now.elapsed());
             return;
         }
-        n += 1;
+        n += 2;
     }
 }
 
-fn is_prime(n: i32) -> bool {
-    for i in 2..n {
-        if n % i == 0 {
-            return false;
-        }
+
+
+fn is_prime(n: u64) -> bool {
+    if n == 2 || n == 3 { return true; }
+    if n < 2 || n % 2 == 0 { return false; }
+    if n < 9 { return true; }
+    if n % 3 == 0 { return false; }
+    let r = f64::sqrt(n as f64) as u64;
+    let mut f = 5;
+    while f <= r {
+        if n % f == 0 { return false; }
+        if n % (f + 2) == 0 { return false; } 
+        f += 6;
     }
     return true;
 }
